bec12443dbb9e4156e7a05cc60b843d0d9064537
Matt Burstein <j.matthew.burstein@gmail.com>
2018-01-07 22:09:29 +0000

    amend account tests to use let() and let!() instead of instance variables

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -12,51 +12,50 @@ describe Account do
   end
 
   context "Once account has been created" do
-    before do
-      @account = Account.new("holder", 100)
-    end
+  #  before do
+      let(:account) {Account.new("holder", 100)}
+  #  end
 
     describe "deposit" do
       it "should increase by deposit amount" do
-        expect{@account.deposit(100)}.to change{@account.balance}.by(100)
+        expect{account.deposit(100)}.to change{account.balance}.by(100)
       end
 
       it "should not increase balance for deposit amount <= 0" do
-        expect{@account.deposit(-1)}.to change{@account.balance}.by(0)
+        expect{account.deposit(-1)}.to change{account.balance}.by(0)
       end
     end
 
     describe "withdraw" do
       it "should decrease by withdrawal amount" do
-        expect{@account.withdraw(100)}.to change{@account.balance}.by(-100)
+        expect{account.withdraw(100)}.to change{account.balance}.by(-100)
       end
 
       it "should not increase balance for deposit amount <= 0" do
-        expect{@account.deposit(-1)}.to change{@account.balance}.by(0)
+        expect{account.deposit(-1)}.to change{account.balance}.by(0)
       end
 
       it "should not allow you to withdraw more than you have" do
-        expect{@account.withdraw(200)}.to change{@account.balance}.by(0)
+        expect{account.withdraw(200)}.to change{account.balance}.by(0)
       end
 
     end
 
     describe "close" do
-      before do
-        @final_balance = @account.balance
-      end
+
+      let!(:final_balance) {account.balance}
 
       it "should return final balance amount" do
-        expect(@account.close).to eq(@final_balance)
+        expect(account.close).to eq(final_balance)
       end
 
       context "once account is closed" do
         before do
-          @account.close
+          account.close
         end
 
         it "should set balance to 0" do
-          expect(@account.balance).to eq(0)
+          expect(account.balance).to eq(0)
         end
       end
     end
